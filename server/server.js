a4b493673a78d4023c8c32ff448b0f5af85e3549
Ashish Salaskar <ashishsalaskar1@gmail.com>
2019-02-04 13:20:41 +0530

    added get /todos/id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 let app = express();
 
@@ -25,12 +26,32 @@ app.post('/todos',(req,res) => {
 
 });
 
+// get.todos
 app.get('/todos',(req,res) => {
     Todo.find().then((todos) => {
         res.send({
             todos
         });
     }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+// get/todos/id
+app.get('/todos/:id',(req,res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        console.log("FAil");
+         res.status(404).send("Id Invalid");
+    }
+
+    Todo.findById(id).then((todo) => {
+        res.send({todo});
+    },(err) => {
+        res.status(404).send();
+    }
+    ).catch((err) => {
         res.status(400).send(err);
     });
 });
